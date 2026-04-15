42dbb736d3493debe2fe809fa2209f796a7345d2
Deepak <deepakkumar62559@gmail.com>
2026-04-16 01:13:57 +0530

    changes in file

--- backend/src/main/java/com/rydora/security/SecurityConfig.java
+++ backend/src/main/java/com/rydora/security/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(
-            List.of("http://localhost:3000"));
+            List.of("http://13.63.58.248"));
         config.setAllowedMethods(
             List.of("GET", "POST", "PUT",
                     "DELETE", "OPTIONS"));
